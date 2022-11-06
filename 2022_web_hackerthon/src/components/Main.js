ca31a370c36ae9e3a676a711caeca79e8c53b023
seongcheol <cher3725@naver.com>
2022-11-06 20:24:20 +0900

    adding main css

--- 2022_web_hackerthon/src/components/Main.js
+++ 2022_web_hackerthon/src/components/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Content from "./Content";
+import wrapper from "../css/Content.module.css";
+import "../css/Main.css";
 
 class Main extends React.Component {
     render() {
         return (
-            <div className="wrapper">
+            <div id="contentWrapper" className={wrapper}>
                 <Content name="여행지"></Content>
                 <Content name="음식점"></Content>
                 <Content name="숙소"></Content>
