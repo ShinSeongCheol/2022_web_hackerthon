935b99bc85c8b8c62429eac0669f5299be44a68b
seongcheol <cher3725@naver.com>
2022-11-06 21:46:42 +0900

    testing (used bootstrap)

---
+++ blog/src/components/Navbar.js
@@ -0,0 +1,36 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#">O₂</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: "100px" }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action2">여행지</Nav.Link>
+                        <Nav.Link href="#action2">음식점</Nav.Link>
+                        <Nav.Link href="#action2">숙소</Nav.Link>
+                        <Nav.Link href="#action2">토론장</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#action">로그인</Nav.Link>
+                        <Nav.Link href="action">회원가입</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default BasicExample;
