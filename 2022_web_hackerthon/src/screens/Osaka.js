b63fb54b0d52ca99ca06ce431549effaf9e37481
seongcheol <cher3725@naver.com>
2022-11-09 20:11:43 +0900

    change layout

---
+++ 2022_web_hackerthon/src/screens/Osaka.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Osaka = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Osaka
---
+++ 2022_web_hackerthon/src/screens/Talk.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Talk = () => {
+  return (
+    <div>
+      <h1>Talk</h1>
+    </div>
+  )
+}
+
+export default Talk
+
