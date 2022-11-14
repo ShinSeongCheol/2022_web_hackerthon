c18f8de44be724dc68c58b8dfc666575799f3c00
shinseongcheol <cher0531@naver.com>
2022-11-15 02:47:14 +0900

    connection test server and client

--- 2022_web_hackerthon/server/server.js
+++ 2022_web_hackerthon/server/server.js
@@ -21,5 +21,9 @@ app.get('/blog', (req, res) => {
     res.send(`Welcome to Sejun's blog`);
 })
 
+app.get('/api/host', (req, res) => {
+    res.send({host:'sejun'});
+})
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Node.js Server is running on port ${PORT}...`));
--- 2022_web_hackerthon/src/screens/Test.js
+++ 2022_web_hackerthon/src/screens/Test.js
@@ -1,89 +1,31 @@
+import axios from 'axios';
+import { Component } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import { Image, Button } from "react-bootstrap";
 
-function Test() {
-    return (
-        <Container
-            fluid
-            className=""
-            style={{
-                backgroundColor: "",
-                height: "",
-            }}
-        >
-            {/*URL(image3/osaka_night.jpg)*/}
-            <Row className="justify-content-md-center">
-                <Col md="auto" style={{ marginTop: "2%" }}>
-                    <h3>오사카</h3>
-                </Col>
-            </Row>
+class Test extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            host:'',
+        }
+    }
 
-            <Row className="justify-content-md-center">
-                <Col md="auto" style={{ marginTop: "" }}>
-                    <p>서일본 최대의 도시!</p>
-                </Col>
-            </Row>
+    componentDidMount() {
+        this._getHost();
+    }
 
-            <div style={{ margin: "1%", padding: "0 5%" }}>
-                <Row xs={1} md={2} lg={2}>
-                    <Col>
-                        <div className="ratio ratio-16x9" style={{ width: "100%", height: "100%" }}>
-                            <iframe
-                                src="https://www.youtube.com/embed/O1sZPY1_pJI"
-                                title="【일본 4K 랜선 산책】 일본 오사카 난바 도톤보리 걸어보기🚶🏻 l 오랜만에 오사카 여행 온 기분 내기"
-                                frameborder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowfullscreen
-                            ></iframe>
-                        </div>
-                    </Col>
+    _getHost = async() => {
+        const res = await axios.get('/api/host');
+        this.setState({host: res.data.host});
+    }
 
-                    <Col>
-                        <div>
-                            <div className="d-inline">
-                                <Image
-                                    src="image2/aburia.png"
-                                    style={{ padding: "5px", borderRadius: "20px", width: "50%" }}
-                                />
-                            </div>
-
-                            <div className="d-inline">
-                                <Image
-                                    src="image2/aburia.png"
-                                    style={{ padding: "5px", borderRadius: "20px", width: "50%" }}
-                                />
-                            </div>
-
-                            <div className="d-inline">
-                                <Image
-                                    src="image2/aburia.png"
-                                    style={{ padding: "5px", borderRadius: "20px", width: "50%" }}
-                                />
-                            </div>
-
-                            <div className="d-inline">
-                                <Image
-                                    src="image2/aburia.png"
-                                    style={{ padding: "5px", borderRadius: "20px", width: "50%" }}
-                                />
-                            </div>
-                        </div>
-                    </Col>
-
-                    {/* <Col>
-                    <Row  className="ratio ratio-16x9">
-                        <Col>
-                            <Image
-                                src="image2/aburia.png"
-                                style={{ padding: "5px", borderRadius: "20px" }}
-                            />
-                        </Col>
-                    </Row>
-                </Col> */}
-                </Row>
+    render() {
+        return(
+            <div>
+                <h3>Welcome to {this.state.host} Blog</h3>
             </div>
-        </Container>
-    );
+        )
+    }
 }
-
 export default Test;
